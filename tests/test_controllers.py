c1d08d8cde13f00161c58190c5e8bfcb6b69035d
MikeWooster <mikewooster87@gmail.com>
2018-11-15 13:55:55 +0000

    Add tests for not allowed methods on endpoints

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -5,9 +5,91 @@ from ledger.accounting import Ledger
 from ledger.accounting_types import get_accounting_type, TypeCode
 
 
-def test_method_not_allowed_for_GET_on_credit_endpoint(clean_session, client):
-    response = client.get("ledger/credit")
-    assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+class MethodNotAllowedTests:
+    allowed_methods = set()
+    endpoint_url = ""
+    default_headers = {}
+    default_data = {}
+
+    def get_endpoint(self):
+        """Returns the requested endpoint.
+
+        Method can be overridden for more complicated use cases.
+        """
+        return self.endpoint_url
+
+    def get_headers(self):
+        return self.default_headers
+
+    def get_data(self):
+        return self.default_data
+
+    def test_GET_method_not_allowed(self, client):
+        if "GET" not in self.allowed_methods:
+            response = client.get(self.get_endpoint())
+            assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+    def test_POST_method_not_allowed(self, client):
+        if "POST" not in self.allowed_methods:
+            response = client.post(
+                self.get_endpoint(),
+                headers=self.get_headers(),
+                json=self.get_data(),
+            )
+            assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+    def test_PUT_method_not_allowed(self, client):
+        if "PUT" not in self.allowed_methods:
+            response = client.put(
+                self.get_endpoint(),
+                headers=self.get_headers(),
+                json=self.get_data(),
+            )
+            assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+    def test_PATCH_method_not_allowed(self, client):
+        if "PATCH" not in self.allowed_methods:
+            response = client.patch(
+                self.get_endpoint(),
+                headers=self.get_headers(),
+                json=self.get_data(),
+            )
+            assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+    def test_DELETE_method_not_allowed(self, client):
+        if "DELETE" not in self.allowed_methods:
+            response = client.delete(
+                self.get_endpoint(),
+                headers=self.get_headers(),
+                json=self.get_data(),
+            )
+            assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+    def test_HEAD_method_not_allowed(self, client):
+        if "HEAD" not in self.allowed_methods:
+            response = client.head(self.get_endpoint())
+            assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+    def test_OPTIONS_method_not_allowed(self, client):
+        if "OPTIONS" not in self.allowed_methods:
+            response = client.options(self.get_endpoint())
+            assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+
+class TestMethodsNotAllowedOnCreditEndpoint(MethodNotAllowedTests):
+    allowed_methods = {"POST", "OPTIONS"}
+    endpoint_url = "/ledger/credit"
+    default_data = {"creditAmount": 1000, "accountNumber": "12340493"}
+
+
+class TestMethodsNotAllowedOnLedgerEndpoint(MethodNotAllowedTests):
+    allowed_methods = {"GET", "OPTIONS", "HEAD"}
+    endpoint_url = "/ledger"
+
+
+class TestMethodsNotAllowedOnBalanceEndpoint(MethodNotAllowedTests):
+    allowed_methods = {"GET", "OPTIONS", "HEAD"}
+    endpoint_url = "/account/12390403/balance"
 
 
 def test_add_credit_to_account_success(clean_session, client):
